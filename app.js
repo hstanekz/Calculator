6b2c2bbecc75b7dc0a5d19022a7c97e1f7580476
Hiwad Stanekzai <Hiwad_stanekzai@yahoo.com>
2023-03-30 17:05:59 -0700

    All buttons working with accurate calculations in place Decimal/Float button debugging required

--- app.js
+++ app.js
@@ -1,31 +1,42 @@
 let num1 = null;
-let num2; null;
+let num2 = null;
+let operator = null;
+let result = null;
 
 
 let plus = document.querySelector(".add");
 let minus = document.querySelector(".subtract");
 let cross = document.querySelector(".multiply");
 let divide = document.querySelector(".divide");
-let clear = document.querySelector(".clear");
-let equals = document.querySelector(".equal");
+let allClear = document.querySelector(".all-clear");
+let equals = document.querySelector(".equals");
+let cancel = document.querySelector(".cancel");
+let decimal = document.querySelector(".decimal");
 
 let display = document.querySelector(".display");
 
-
+//mathematical functions
 function add(num1, num2) {
-    display.textContent = num1 + num2;
+    result = num1 + num2;
+    display.textContent = result;
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    result = num1 - num2;
+    display.textContent = result;
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    result = num1 * num2;
+    display.textContent = result;
 }
 
 function division(num1, num2) {
-    return num1 / num2;
+    if (num2 == 0) {
+        result = "NaN";
+}
+    else {result = num1 / num2;}
+    display.textContent = result; 
 }
 
 
@@ -41,31 +52,85 @@ function operate(operator, num1, num2) {
             multiply(num1, num2);
             break;
         case '/':
-            divide(num1, num2);
+            division(num1, num2);
             break;
     }
 
 }
 
+//button selectors
 for (let i = 0; i < 10; ++i) {
     let numBtn = document.getElementById(`num${i}`);
     numBtn.addEventListener('click', () => {
-        if (num1 == null) {
-            num1 = display.textContent += i;
+        display.textContent += i;
+        if (operator === null) { 
+            if (num1 === null) {
+                num1 = Number(display.textContent);
+            } else {
+                num1 = Number(num1.toString() + i.toString()); //concatenate strings then revert to integers
+            }
+        } else { //operator has been assigned
+            if (num2 === null) {
+                num2 = Number(display.textContent);
+            } else {
+                num2 = Number(num2.toString() + i.toString());
+            }
         }
-        else if (num2 == null) {
-            num2 = display.textContent +=i;
-        }
-    })
+    });
 }
 
 
-clear.addEventListener('click', () => {
-    display.textContent = 0;})
+allClear.addEventListener('click', () => {
+    display.textContent = "";
+    num1 = null;
+    num2 = null;
+    operator = null;
+    })
 plus.addEventListener('click', () => {
+    display.textContent = "+"
     operator = "+";
 })
+minus.addEventListener('click', () => {
+    display.textContent = ""
+    operator = "-";
+
+})
+cross.addEventListener('click', () => {
+    display.textContent = ""
+    operator = "*";
+
+})
+divide.addEventListener('click', () => {
+    display.textContent = ""
+    operator = "/";
+
+})
+cancel.addEventListener('click', () => {
+    if (operator == null) {
+    display.textContent = display.textContent.slice(0,-1);
+    num1 = display.textContent; }
+    else {
+        display.textContent = display.textContent.slice(0,-1);
+    num2 = display.textContent;
+    }
+})
+decimal.addEventListener('click', () => {
+    if (operator == null && !num1.toString().includes(".")) {
+        display.textContent += '.';
+        num1 = Number(num1.toString() + '.');
+    }
+    else if (operator != null && !num2.toString().includes(".")) {
+        display.textContent += '.';
+        num2 = Number(num2.toString() + '.');
+    }})
 
 equals.addEventListener('click', () => {
-    if (num1 != null && num2 != null & operator != null) 
-    operate(operator, num1, num2)})
+    if (num1 != null && num2 != null && operator != null) {
+        console.log(Number(num1)) 
+        console.log(Number(num2))
+    operate(operator, num1, num2);
+    num1 = result;
+    num2 = null;
+    operator = null;
+
+}});
